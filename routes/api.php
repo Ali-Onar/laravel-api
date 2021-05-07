74e932aa671cdc6e79f3f0c79ab9b836ea4f3c1f
= <alitunacanonar@icloud.com>
2021-05-07 21:36:53 +0300

    laravel installation and first api request made.

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function (Request $request) {
+    $user = User::find(1);
+
+    return [
+        'name' => $user->name,
+        'email' => $user->email,
+        'created' => $user->created_at
+    ];
+});

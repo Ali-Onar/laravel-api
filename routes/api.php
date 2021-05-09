cbaf7c3a7c352d893e67863c7e73c95f4ab0ce3a
= <alitunacanonar@icloud.com>
2021-05-09 15:51:54 +0300

    jetstream and sanctum were established. sanctum made for api.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,27 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/movies', MovieController::class);
+Route::apiResource('/movies', MovieController::class)->middleware('auth:sanctum');
+
+
+Route::post('/tokens/create', function (Request $request) {
+    $request->validate([
+        'email' => 'required',
+        'password' => 'required'
+    ]);
+
+    $user = User::whereEmail($request->email)->first();
+
+    if ($user) {
+        $token = $user->createToken('movies');
+        return ['token' => $token->plainTextToken];
+    }
+});
